894418f8f6e4ec611456bc602af7c995809ce2b1
Alfred Sorrow <kurtsorrow@gmail.com>
2023-08-30 14:45:19 +0400

    authorization, registration, hooks

---
+++ app/src/Controller/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sonata\AdminBundle\Templating\TemplateRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, TemplateRegistry $templateRegistry): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $user->setEnabled(true);
+            $user->setRoles(['ROLE_ADMIN']);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('registration_success', 'Your account has been created. You can now log in.');
+
+            return $this->redirectToRoute('sonata_user_admin_security_login');
+        }
+
+        return new Response($this->renderView('auth/register.html.twig', [
+            'errors' => $form->getErrors(true),
+            'base_template' => $templateRegistry->getTemplate('layout'),
+            'registrationForm' => $form->createView(),
+        ]));
+    }
+}
